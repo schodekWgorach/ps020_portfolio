727c5d91591d75bba904c243eba5fc1fd2839275
schodekWgorach <sschodnicki@gmail.com>
2025-01-27 17:44:09 +0100

    main scss, variables

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <header className="header">
+        <a href="#home" class='logo'>Szymon
+          <span>Schodnicki</span></a> 
+
+          <nav className="navbar"></nav>
+           
+      </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
