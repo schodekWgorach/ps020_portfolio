1369752c5773a6c531cb6d53df41910cf1aa1d07
schodekWgorach <sschodnicki@gmail.com>
2025-01-27 23:22:11 +0100

    App + main.scss modification

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@
 function App() {
   return (
     <div className="App">
-      <div className="container">
-      <header className="header">
+      <div class="container">
+      <header class="header">
         <a href="#home" class='logo'>Szymon
           <span>Schodnicki</span></a> 
 
-          <nav className="navbar"></nav>
+          <nav href='#home' className="navbar">
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#education">Edukation</a></li>
+              <li><a href="#technologis">Technologis</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul> 
+
+            <div className="search-bar">
+              <input type="text" placeholder="Search..." />
+              <button>Search</button>
+            </div>
+
+      
+          </nav>
            
       </header>
       </div>
