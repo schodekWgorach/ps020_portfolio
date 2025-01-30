602b459a92297feddf03f11792a0a6fd4ae7f9fd
schodekWgorach <sschodnicki@gmail.com>
2025-01-30 16:07:43 +0100

    home2

--- src/App.jsx
+++ src/App.jsx
@@ -26,41 +26,47 @@ function App() {
       
       {/* home */}
       <section className="home" id='home'>
-        <div className="home__content">
-        <h1 className="home__content-h1">Hi, it's<span></span></h1>
-        <h3 className="home__content-animation">i'm a</h3>
+
+        <div className="home__container">
+
+          <div className="home__content">
+
+                <h1 className="home__content-h1">Hi, it's<span> Szymon</span></h1>
+                <h3 className="home__content-animation">I'm a</h3>
+                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maiores doloremque eaque aspernatur incidunt saepe 
+                  temporibus ex reprehenderit similique est ipsa repudiandae,
+                  eligendi mollitia? Atque neque sunt magni commodi quaerat odio?</p>
         
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Maiores doloremque eaque aspernatur incidunt saepe 
-           temporibus ex reprehenderit similique est ipsa repudiandae,
-            eligendi mollitia? Atque neque sunt magni commodi quaerat odio?</p>
-        </div>
-        <div className="content__socialicon">
-          <a href="https://www.linkedin.com/in/szymon-schodnicki-7798b6256/" id="likedin">1</a>
-          <a href="mail" id="gmail">2</a>
-          <a href="https://github.com/schodekWgorach?tab=overview&from=2025-01-01&to=2025-01-29" id="github">3</a>
 
-          <div className="home__button">
-            <a href="#github" className="button">github</a>
-            <a href="#contakt" className="button">Kontakt</a>
-          </div>
- 
+            <div className="content__socialicon">
+              <a href="https://www.linkedin.com/in/szymon-schodnicki-7798b6256/" id="likedin">1</a>
+              <a href="mail" id="gmail">2</a>
+              <a href="https://github.com/schodekWgorach?tab=overview&from=2025-01-01&to=2025-01-29" id="github">3</a>
 
-
+            
+            <div className="home__button">
+              <a href="#github" className="button">github</a>
+              <a href="#contakt" className="button">Kontakt</a>
+            </div>
+            
+  
+          
+            </div>
+            </div>
           <div className="home__image">
           <img src={faceImage} alt="zdięcie profilowe" />
-          </div>
-        
+          
           
         </div>
+        
+          
+      </div>
         
 
       </section>
       {/* home */}
-<div className="blur-element">hfdhfsdkajh</div>
-<div className='blur-text'></div>
-
-
 
 
 
